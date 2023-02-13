763bc5305f75704086a94b64354c9e86c8e5ea21
ranss@rambler.ru <ranss@rambler.ru>
2023-02-13 23:15:53 +0300

    коммит перед 8 задачей

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,13 @@ public class Main {
         System.out.println("Разница между весами боксеров " + weightDifference + "кг.");
         var weightDifference2 = (boxer1 + boxer2) % boxer1;
         System.out.println("Разница между весами боксеров 2 вариант расчета " + weightDifference2 + "кг.");
+        var totalTime = 640;
+        var time1 = 8;
+        var amountOfWorkers = totalTime / time1;
+        System.out.println("Всего работников в компании " + amountOfWorkers + " человек");
+        amountOfWorkers = amountOfWorkers + 94;
+        var totalTime2 = amountOfWorkers * time1;
+        System.out.println("Если в компании работает " + amountOfWorkers + " человек, то всего " + totalTime2 + " часов работы может быть поделено между сотрудниками");
 
 
     }
