c4841f2234a529a6d06073143845fbb3d65434f2
ranss@rambler.ru <ranss@rambler.ru>
2023-02-14 10:23:48 +0300

    Итоговый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        System.out.println("Hello world!");
         var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
